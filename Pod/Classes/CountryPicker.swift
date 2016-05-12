0332195a9dca6be19ab2acf6b550a88a388f472d
Jesse Armand <jesse@jessearmand.com>
2016-05-12 16:40:13 +0800

    Improvements on country data
    * Loading flag images instead of emoji
    * Added country dialing code
    * Deployment target can be iOS 8.0

--- Pod/Classes/CountryPicker.swift
+++ Pod/Classes/CountryPicker.swift
@@ -3,108 +3,128 @@
     Country object
     contains a name, iso country code and emoji, all strings.
 */
+
 public struct Country {
-    
+
     /// Name of the country
-    public let name : String!
-    
+    public let name: String!
+
     /// ISO country code of the country
-    public let iso : String!
-    
+    public let iso: String!
+
     /// Emoji flag of the country
     public let emoji: String!
-    
+
+    /// Dialing code of the country
+    public let dial: String!
+
+    /// Flag image of the country
+    public let flag: UIImage!
 }
 
 
-public protocol CountryPickerDelegate : UIPickerViewDelegate {
-    
+public protocol CountryPickerDelegate: UIPickerViewDelegate {
+
     /**
      Called by the CountryPicker when the user selects a country
-     
+
      - parameter picker:  An object representing the CounrtyPicker requesting the data.
      - parameter country: The Selected Country
      */
     func countryPicker(picker: CountryPicker, didSelectCountry country: Country)
 }
 
-/** 
+/**
  The CountryPicker class uses a custom subclass of UIPickerView to display country names and flags (emoji flags) in a slot machine interface. The user can choose a pick a country.
 */
-public class CountryPicker : UIPickerView {
-  
+public class CountryPicker: UIPickerView {
+
     /// The current picked Country
-    public var pickedCountry : Country?
-    
+    public var pickedCountry: Country?
+
     /// The delegate for the CountryPicker
-    public var countryDelegate : CountryPickerDelegate?
-    
+    public var countryDelegate: CountryPickerDelegate?
+
     /// The Content of the CountryPicker
     private var countryData = [Country]()
-    
-  
+
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         self.dataSource = self
         self.delegate = self
+
+        self.backgroundColor = .whiteColor()
+
         loadData()
     }
 
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     /**
      Loads content from .json file
      */
     private func loadData() {
         let bundlePath = NSBundle(forClass: CountryPicker.self).pathForResource("SwiftCountryPicker", ofType: "bundle")
-        
-        if let path = NSBundle(path: bundlePath!)!.pathForResource("EmojiCountryCodes", ofType: "json")
-        {
-            
+
+        if let path = NSBundle(path: bundlePath!)!.pathForResource("EmojiCountryCodes", ofType: "json") {
+
             do {
                 let jsonData = try NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe)
                 let json = try NSJSONSerialization.JSONObjectWithData(jsonData, options: .AllowFragments)
 
                 var countryCode: String?
- 
+
                 if let local = NSLocale.currentLocale().objectForKey(NSLocaleCountryCode) as? String {
                     countryCode = local
                 }
-                
-                
-                guard let countries = json as? NSArray else {
+
+
+                guard let rawCountries = json as? NSArray else {
                     print("countries is not an array")
                     return
                 }
-                
-                for subJson in countries{
-                    
-                    guard let name = subJson["name"] as? String, iso = subJson["code"] as? String, emoji = subJson["emoji"] as? String else {
-                        
+
+                let countries = rawCountries.filter { country in
+                    guard let dial = country["dial"] as? String else {
+                        return false
+                    }
+
+                    return dial.isEmpty == false
+                }
+
+                for subJson in countries {
+
+                    guard let name = subJson["name"] as? String,
+                        iso = subJson["code"] as? String,
+                        emoji = subJson["emoji"] as? String,
+                        dial = subJson["dial"] as? String else {
+
                         print("couldn't parse json")
-                        
+
                         break
                     }
-                    
-                    let country = Country(name: name, iso: iso, emoji: emoji)
-                    
-                    // set current country if it's the local countr y
+
+                    let image = UIImage(named: iso, inBundle: NSBundle(path: bundlePath!)!, compatibleWithTraitCollection: nil)
+                    let country = Country(name: name, iso: iso, emoji: emoji, dial: dial, flag: image)
+
+                    // set current country if it's the local country
                     if country.iso == countryCode {
                         pickedCountry = country
                     }
-                    
+
                     // append country
                     countryData.append(country)
                 }
-                
+
                 countryData.sortInPlace { $1.name > $0.name }
                 self.reloadAllComponents()
-                
+
             } catch {
                 print("error reading file")
-                
+
             }
         }
 
@@ -112,15 +132,66 @@ public class CountryPicker : UIPickerView {
 }
 
 extension CountryPicker : UIPickerViewDataSource {
-    
-    public func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "\(countryData[row].emoji) - \(countryData[row].name)"
+
+    var flagTag: Int { return 1 }
+    var countryNameTag: Int { return 2 }
+
+    public func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
+        let flag = countryData[row].flag
+        let name = countryData[row].name
+        let dial = countryData[row].dial
+
+        let text = "\(name) +\(dial)"
+        let mutableAttributedText = NSMutableAttributedString(string: text, attributes: [NSFontAttributeName: UIFont.systemFontOfSize(UIFont.labelFontSize())])
+        let range = NSString(string: mutableAttributedText.string).rangeOfString("+\(dial)")
+        mutableAttributedText.setAttributes([NSForegroundColorAttributeName: UIColor.lightGrayColor()], range: range)
+
+        let countryNameAndDial = NSAttributedString(attributedString: mutableAttributedText)
+
+        guard let rowView = view else {
+            let rowView = UIView(frame: bounds)
+
+            let flagView = UIImageView(image: flag)
+            flagView.translatesAutoresizingMaskIntoConstraints = false
+            flagView.tag = flagTag
+            rowView.addSubview(flagView)
+
+            let countryNameLabel = UILabel()
+            countryNameLabel.translatesAutoresizingMaskIntoConstraints = false
+            countryNameLabel.tag = countryNameTag
+            countryNameLabel.attributedText = countryNameAndDial
+
+            rowView.addSubview(countryNameLabel)
+
+            let flagWidthOrHeight = 24
+            let views = ["flagView": flagView, "countryNameLabel": countryNameLabel]
+            let horizontalFormat = "|-8-[flagView(\(flagWidthOrHeight))]-[countryNameLabel]-|"
+            let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(horizontalFormat, options: .AlignAllCenterY, metrics: nil, views: views)
+
+            let verticalFormat = "V:[flagView(\(flagWidthOrHeight))]"
+            let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(verticalFormat, options: .AlignAllCenterY, metrics: nil, views: ["flagView": flagView])
+
+            NSLayoutConstraint.activateConstraints(horizontalConstraints)
+            NSLayoutConstraint.activateConstraints(verticalConstraints)
+
+            return rowView
+        }
+
+        if let flagView = rowView.viewWithTag(flagTag) as? UIImageView {
+            flagView.image = flag
+        }
+
+        if let countryNameLabel = rowView.viewWithTag(countryNameTag) as? UILabel {
+            countryNameLabel.attributedText = countryNameAndDial
+        }
+
+        return rowView
     }
-    
+
     public func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return countryData.count
     }
-    
+
     public func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -137,4 +208,3 @@ extension CountryPicker : UIPickerViewDelegate {
     }
 
 }
-
--- SwiftCountryPicker.podspec
+++ SwiftCountryPicker.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = "SwiftCountryPicker"
-  s.version          = "1.0.0"
+  s.version          = "1.1.0"
   s.summary          = "A country picker that uses EMOJI as flags in Swift."
   s.description      = <<-DESC
     A very simple CountryPicker in Swift, which is super easy to implement. It uses emoji as flags, so it's size is as low as possible. When a user has selected a country you can retreieve it's name and flag, but most importantly: it's ISO code. It's a subclass UIPickerView that displays country names and flags in a slot machine interface. Customization is very easy, because it's a UIPickerView suvlcass.
@@ -13,12 +13,12 @@ Pod::Spec.new do |s|
   s.source           = { :git => "https://github.com/samuelbeek/SwiftCountryPicker.git", :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/samuelbeek'
 
-  s.platform     = :ios, '9.0'
+  s.platform     = :ios, '8.0'
   s.requires_arc = true
 
   s.source_files = 'Pod/Classes/**/*'
   s.resource_bundles = {
-    'SwiftCountryPicker' => ['Pod/Assets/*.json']
+    'SwiftCountryPicker' => ['Pod/Assets/*.json', 'Pod/Assets/flags/*.png']
   }
   s.frameworks = 'UIKit'
 end
